1cfb6176799bed2a5309d2ec17a3b8ed6182e532
Joe Bailey-Roberts <dev@jbr.digital>
2023-01-24 19:53:15 +0000

    Setup

---
+++ wp-config.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the installation.
+ * You don't have to use the web site, you can copy this file to "wp-config.php"
+ * and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * Database settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ */
+
+if ( is_readable( './.config/production.php' ) ) {
+	require_once( '/.config/production.php ' );
+} else {
+	require_once( './.config/local.php' );
+}
+
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', __DIR__ . '/' );
+}
+
+/** Sets up WordPress vars and included files. */
+require_once ABSPATH . 'wp-settings.php';
